0a8fa63f122f6c81e1b819964de594697ad1b6fd
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2024-12-08 10:12:55 -0300

    alter package and server

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,7 @@ const port = process.env.PORT ?? 4000;
 app.use(express.json());
 app.use(cors());
 app.use(urlencoded({extended: false}));
-
-app.get('/', (res: Response, req: Request) => {
-      res.json({pong: true});
-})
+app.use(Router());
 
 app.listen(port || 4000, () => {
     console.log(`Server: http://localhost:${port}`);
