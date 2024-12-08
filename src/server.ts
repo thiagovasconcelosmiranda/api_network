4a3a56d2eadd02a1df0453c37dabf705a1e346a8
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2024-12-08 10:28:18 -0300

    n

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ const port = process.env.PORT ?? 4000;
 app.use(express.json());
 app.use(cors());
 app.use(urlencoded({ extended: false }));
-app.use(router);
 
 app.get('/', (res: Response, req: Request)=>{
    res.json({test: true});
